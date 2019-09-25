b3dd768ecf0a9a3203569e311cdd9908c6a4dd26
tylerloh <tyleryin811@gmail.com>
2019-09-25 13:52:48 +0800

    Remove unuse directory

--- demosite/apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "user"
-urlpatterns = [
-    path('login/', views.LoginView.as_view(), name="login"),
-    path('logout/', views.user_logout, name='logout'),
-]
